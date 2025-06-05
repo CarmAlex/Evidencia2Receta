2f68d6f6c468dc89ec3091eac69f5d119f1d1316
Carlos Olivarez <carmanolmon@gmail.com>
2025-06-05 15:47:34 -0600

    Font y colores

--- src/evidencia2receta/core.clj
+++ src/evidencia2receta/core.clj
@@ -1,1 +1,20 @@
-(ns core)
+
+(ns evidencia2receta.core)
+
+;; --- Mapa de colores y estilos (similar al hash de Racket) ---
+(def colores
+  {:quantity  "color: yellow; font-weight: bold;"
+   :ingredient "color: green; font-style: italic;"
+   :step-number "color: cyan; font-weight: bold;"
+   :temperature "color: red; text-decoration: underline;"
+   :category "color: green; text-transform: uppercase;"
+   :equipment "color: blue;"
+   :portions "color: magenta; font-weight: bold;"
+   :action "color: orange; font-style: italic;"
+   :note "color: red; font-weight: bold; text-decoration: underline;"
+   :default ""})
+
+;; --- Función para envolver texto con estilo usando span ---
+(defn span-con-estilo [texto tipo]
+  (let [estilo (get colores tipo "")]
+    (str "<span style=\"" estilo "\">" texto "</span>")))
