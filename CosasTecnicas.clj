84261b1201871714368fb1422f9332a8742b7278
CarmAlex <a01198988@tec.mx>
2025-06-05 15:54:18 -0600

    prub conversiones corregida

--- CosasTecnicas.clj
+++ CosasTecnicas.clj
@@ -16,12 +16,18 @@
 ;Taza a gramos dependiendo del ingrediente
 (defn tazaAg [ing]
   (let [cantidad (parse-fraction (first ing))
-        ingrediente (nth ing 2) conversiones {"granulated sugar" 200
+        ingrediente (nth ing 2) 
+        conversiones {"granulated sugar" 200
                       "all-purpose flour" 125
                       "cocoa powder" 151
                       "powdered sugar" 120
                       "butter" 227
                       "water" 236}           
-        gramos (get conversiones ingrediente)] 
+        gramos (get conversiones ingrediente 0)]
+        (if gramos (* cantidad gramos)
+        "ingrediente no encontrado"
+        )
   )
 )
+
+(map tazaAg listaChida)
