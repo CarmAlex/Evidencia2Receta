58e29169c4a64b6764857de10972f12ecefde0de
CarmAlex <a01198988@tec.mx>
2025-06-05 15:39:25 -0600

    prubconversiones

--- CosasTecnicas.clj
+++ CosasTecnicas.clj
@@ -1,9 +1,27 @@
 
+; Conversión de tazas a gramos
+(def listaChida '(("1/2" "cups" "granulated sugar") 
+                  ("1" "cups" "all-purpose flour") 
+                  ("3/4" "cups" "butter")))
 
-;funcion para hacer que las fracciones se hagan en cadenas 
+;funcion para hacer que las fracciones se hagan de cadenas en decimales 
 (defn parse-fraction [frac]
   (let [parts (clojure.string/split frac #"/")]
     (if (= (count parts) 1)
       (Double/parseDouble (first parts))
       (/ (Double/parseDouble (first parts)) 
          (Double/parseDouble (second parts))))))
+
+
+;Taza a gramos dependiendo del ingrediente
+(defn tazaAg [ing]
+  (let [cantidad (parse-fraction (first ing))
+        ingrediente (nth ing 2) conversiones {"granulated sugar" 200
+                      "all-purpose flour" 125
+                      "cocoa powder" 151
+                      "powdered sugar" 120
+                      "butter" 227
+                      "water" 236}           
+        gramos (get conversiones ingrediente)] 
+  )
+)
