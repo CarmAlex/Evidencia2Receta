4dcfd2a4dffdfd2dad43f39fe69a3a7bd620ecca
Carlos Olivarez <carmanolmon@gmail.com>
2025-06-06 10:38:43 -0600

    estilos y leer archivo

--- src/evidencia2receta/core.clj
+++ src/evidencia2receta/core.clj
@@ -4,18 +4,36 @@
   {:quantity  "color: yellow; font-weight: bold;"
    :ingredient "color: green; font-style: italic;"
    :step-number "color: cyan; font-weight: bold;"
-   :temperature "color: red; text-decoration: underline;"
-   :category "color: green; text-transform: uppercase;"
+   :temperature "color: red; text-decoration: underline;" 
    :equipment "color: blue;"
-   :portions "color: magenta; font-weight: bold;"
-   :action "color: orange; font-style: italic;"
-   :note "color: red; font-weight: bold; text-decoration: underline;"
+   :serves "color: magenta; font-weight: bold;" 
    :default ""})
 
 ;; --- Función para envolver texto con estilo usando span ---
 (defn span-con-estilo [texto tipo]
   (let [estilo (get colores tipo "")]
     (str "<span style=\"" estilo "\">" texto "</span>")))
+
+(def (leer-archivo [ruta]
+  (with-open [rdr (clojure.java.io/reader ruta)]
+    (doall (line-seq rdr)))))
+
+;; --- Función para procesar una receta y aplicar estilos ---
+(defn procesar-receta [receta]
+  (let [lineas (clojure.string/split-lines receta)]
+    (mapv
+      (fn [linea]
+        (cond
+          (re-matches #"\d+\.\s.*" linea) (span-con-estilo linea :step-number)
+          (re-matches #"\d+\s.*" linea) (span-con-estilo linea :quantity)
+          (re-matches #".*:\s.*" linea) (span-con-estilo linea :ingredient)
+          (re-matches #".*:\s.*" linea) (span-con-estilo linea :temperature)
+          (re-matches #".*:\s.*" linea) (span-con-estilo linea :equipment)
+          (re-matches #".*serves\s\d+" linea) (span-con-estilo linea :serves)
+          :else (span-con-estilo linea :default)))
+      lineas)))
+
+
 
 (println
 (slurp "../../resources/Lemon Cake-1.txt"))
