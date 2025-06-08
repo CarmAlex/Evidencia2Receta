bed86a6dcda93cac0cefa3595ae209e4708f5ef7
CarmAlex <a01198988@tec.mx>
2025-06-07 19:02:40 -0600

    prubSettings

--- settings.clj
+++ settings.clj
@@ -3,4 +3,13 @@
 ; sistema: metrico o tazas(incluye tsp y tbsp)
 ; temperatura: Fahrenheit o Celsius
 ; porciones: 
-; filtro: all, dessert, 
+; filtro: all, dessert, savory
+
+; funcion para leer si se usa un sistema metrico u otro
+(defn metrOtz [opcion]
+    (if (= opcion "metro")
+        (println "si")
+        (println "no")
+    )
+)
+
